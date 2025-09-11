be3557a9c6df02458dfbee9bcce3fcdc43bc2d67
Кирилл Семенихин <ska_ferro@list.ru>
2025-09-12 03:18:36 +0700

    add function in processing

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+def filter_by_state(banking_operations, state='EXECUTED'):
+    """ Функция принимает на вход список словарей с данными о банковских операциях и параметр state,
+    возвращает новый список, содержащий только те словари,
+    у которых ключ state содержит переданное в функцию значение. """
+
+    return [operation for operation in banking_operations if operation.get('state') == state]
+
+
+def sort_by_date(banking_operations, reverse=True):
+    """ Функция принимает на вход список словарей и параметр порядка сортировки,
+    возвращает новый список, в котором исходные словари отсортированы по дате."""
+
+    return sorted(banking_operations, key=lambda x: x.get('date'), reverse=reverse)
