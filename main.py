5e9af5987f019301466875e31281ba50aa5ae933
Кирилл Семенихин <ska_ferro@list.ru>
2025-09-15 21:55:58 +0700

    fixed

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
-x = 10
-y = 15
-print(x + y)
 
-
-def sum_of_values(value_1, value_2):
-    """
-    Функция, суммирующая два числа
-    """
-    summa = value_1 + value_2
-    return summa
-
-
-def sub_of_values(value_1, value_2):
-    """
-    Функция, вычитающая два числа
-    """
-    subtract = value_1 - value_2
-    return subtract
