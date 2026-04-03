9b2061d99da0169f5fd93cde345ddb36f3098264
Кирилл Семенихин <ska_ferro@list.ru>
2026-04-04 04:50:37 +0700

    add main

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import json
 import logging
+import re
 from typing import Any, Dict, List
+
+import numpy
 
 logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
@@ -31,6 +34,38 @@ def load_transactions(file_path: str) -> List[Dict[str, Any]]:
         logger.error(f"Произошла ошибка: {error}. Вернем пустой список.")
         return []
 
+
+def filter_transactions_by_description(transactions, search_string):
+        """Фильтрует транзакции по описанию."""
+
+        pattern = re.compile(re.escape(search_string), re.IGNORECASE)
+        return [transaction for transaction in transactions if pattern.search(transaction['description'])]
+
+
+def count_operations_by_category(transactions, categories):
+        """Считает количество операций по категориям."""
+        category_count = {category: 0 for category in categories}
+        for transaction in transactions:
+            for category in categories:
+                if category in transaction['description']:
+                    category_count[category] += 1
+        return category_count
+
+
+def extract_values_transactions(data, key):
+    """
+    Функция принимает список словарей с данными о банковских операциях и значение ключа,
+    а возвращает список категорий операций (Категории операций из поля 'description')
+    """
+    # Собираем все значения в список
+    transactions = [item['description'] for item in data if 'description' in item]
+
+    # Удаляем пустые значения из списка
+    cleaned_category_operations = [transaction for transaction in transactions if transaction is not numpy.nan]
+
+
+    # Возвращаем уникальные значения с помощью множества
+    return list(set(cleaned_category_operations))
 
 if __name__ == "__main__":
 
